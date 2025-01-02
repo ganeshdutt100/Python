3da57149a8dca547efedc36ebe711195a7028426
unknown <gdutt2304@gmail.com>
2025-01-03 01:45:40 +0530

    first commit

---
+++ variabes.py
@@ -0,0 +1,11 @@
+# a = 12
+# b = 34.53
+# name = "34.43"
+# t= float(name)
+# ty = type(t)
+# print(ty)
+# print(a + b , name  )
+
+a = int(input("Enter your first number : "))
+b = int(input("Enter your second number : "))
+print("Sum of a and b : ", a+b)
