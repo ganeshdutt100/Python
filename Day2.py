4b7b46288654b61601a3e3a0a1d67f42b7870149
unknown <gdutt2304@gmail.com>
2025-01-03 13:16:08 +0530

    Update String

---
+++ Day2.py
@@ -0,0 +1,28 @@
+# a = "Hello world"
+# print(a)
+
+# name = input("Enter your name ")
+# print("Hello "+ name +  " How are you ")
+
+# number = int(input("Enter your weekDays number "))
+
+# days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+# if(number == 1 & number <= 5):
+#     print("weekDays")
+
+# elif(number == 6 & number <= 7):
+#     print("weekend")print("python")
+# num1 = int(input("Enter your first number : "))
+# num2 = int(input("Enter your second number : "))
+# print(" Sum of num1 and num2 : " , (num1 + num2)) 
+# print("Power of num1 ", num1**3)
+ 
+
+name = "Ganesh"
+# nameShort = name[-1:-4]
+nameShort = name[0:3]
+nameShort = name[-4:-1]
+nameShort = name[:]
+
+print(nameShort)
