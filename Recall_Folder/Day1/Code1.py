c37dbd6defbdf54116ed9a28d4ed6cd914366421
Ganesh Dutt <gdutt2304@gmail.com>
2025-01-21 18:39:29 +0530

    Update recall folder

---
+++ Recall_Folder/Day1/Code1.py
@@ -0,0 +1,34 @@
+print("Hello world!")
+# print("Please Enter your name : ")
+# name  = input("What is your name :  ")
+# print("Hello " + name +"! How are you?")
+
+
+a = 12.5
+print(type(a))
+
+
+# age  = int(input("Enter your age : "))
+# if age <= 18:
+#     print("You are bachha ")
+# else:
+#     print("You are bada bachhe")
+
+    #String 
+str = '''
+            hey 
+    guys 
+    my 
+    name 
+    is 
+    Ganesh 
+    Dutt
+            ''' 
+print(str)
+
+# String Operations
+greeting  =  "Hello "+ "" + "Ganesh"
+print(len(greeting))
+
+repetition = ("Hello " * 5) + "\n"
+print(repetition)
